c0b97efbf6439f9fb874b0bab82874dc4fc2d774
Sarah Vessels <cheshire137@gmail.com>
2019-04-06 11:28:01 -0500

    Add message when list is empty

--- src/components/WordList.js
+++ src/components/WordList.js
@@ -16,41 +16,45 @@ class WordList extends Component {
             onChange={onSortChange}
           />
         </div>
-        <ul>
-          {words.map(word => (
-            <li key={word.value}>
-              <div className="mb-2">
-                {word.value}
-                {word.translation ? (
-                  <span>
-                    &mdash; {word.translation}
-                  </span>
-                ) : null}
-                <div>
-                  <span className="Label mr-2 Label--gray">{word.pinyin}</span>
-                  {word.type === 'noun' ? (
-                     <span className="text-small text-gray">n.</span>
-                   ) :
-                   word.type === 'verb' ? (
-                     <span className="text-small text-gray">v.</span>
-                   ) :
-                   word.type === 'adjective' ? (
-                     <span className="text-small text-gray">adj.</span>
-                   ) :
-                   word.type === 'adverb' ? (
-                     <span className="text-small text-gray">adv.</span>
-                   ) :
-                   word.type === 'pronoun' ? (
-                     <span className="text-small text-gray">pro.</span>
-                   ) :
-                   word.type === 'measure' ? (
-                     <span className="text-small text-gray">measure</span>
-                   ) : null}
+        {words.length > 0 ? (
+          <ul>
+            {words.map(word => (
+              <li key={word.value}>
+                <div className="mb-2">
+                  {word.value}
+                  {word.translation ? (
+                    <span>
+                      &mdash; {word.translation}
+                    </span>
+                  ) : null}
+                  <div>
+                    <span className="Label mr-2 Label--gray">{word.pinyin}</span>
+                    {word.type === 'noun' ? (
+                       <span className="text-small text-gray">n.</span>
+                     ) :
+                     word.type === 'verb' ? (
+                       <span className="text-small text-gray">v.</span>
+                     ) :
+                     word.type === 'adjective' ? (
+                       <span className="text-small text-gray">adj.</span>
+                     ) :
+                     word.type === 'adverb' ? (
+                       <span className="text-small text-gray">adv.</span>
+                     ) :
+                     word.type === 'pronoun' ? (
+                       <span className="text-small text-gray">pro.</span>
+                     ) :
+                     word.type === 'measure' ? (
+                       <span className="text-small text-gray">measure</span>
+                     ) : null}
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>You haven't added any learned words or phrases yet.</p>
+        )}
       </div>
     );
   }
