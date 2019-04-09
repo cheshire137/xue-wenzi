f1ae64c0030d4b23f8f99d340af98c69fed12319
Sarah Vessels <cheshire137@gmail.com>
2019-04-08 20:08:58 -0500

    Updates

---
+++ precache-manifest.e8e088449fccd75524a774415c4b9228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "821510cf38e288c9a63a",
+    "url": "/xue-wenzi/static/js/runtime~main.821510cf.js"
+  },
+  {
+    "revision": "88b693afde9bf6fed69c",
+    "url": "/xue-wenzi/static/js/main.88b693af.chunk.js"
+  },
+  {
+    "revision": "a462f3763d16516d3e42",
+    "url": "/xue-wenzi/static/js/2.a462f376.chunk.js"
+  },
+  {
+    "revision": "88b693afde9bf6fed69c",
+    "url": "/xue-wenzi/static/css/main.2278202a.chunk.css"
+  },
+  {
+    "revision": "cde8098f594d950eef1d29cbb3da916c",
+    "url": "/xue-wenzi/index.html"
+  }
+];
